01b87ce9e44ece049cd8efcb99d4ab620ad87082
Junior Love <107013570+junlov@users.noreply.github.com>
2023-03-17 11:04:13 -0400

    Completed Exercises 4.3-4.5, Pass 4.6 & 4.7 - TODO

---
+++ utils/list_helper.js
@@ -0,0 +1,96 @@
+const _ = require("lodash");
+
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0
+    ? {}
+    : blogs.reduce(
+        (blogWithMaxLikes, blog) =>
+          blog.likes > blogWithMaxLikes.likes ? blog : blogWithMaxLikes,
+        blogs[0]
+      );
+};
+
+const mostBlogs = (blogs) => {
+  return blogs.length === 0 ? 0 : {};
+
+  // TODO - 4.6 https://fullstackopen.com/en/part4/structure_of_backend_application_introduction_to_testing#exercises-4-3-4-7
+  // Iterate through array of blogs
+  // Add author name to an object array - don't allow for duplicates
+  // Each time author name is added to the array, add as a count to it's object
+  // Do a comparison of each object to see which author has the most blogs
+  // Return the author with the most blogs
+};
+
+const mostLikes = (blogs) => {
+  return blogs.length === 0 ? 0 : {};
+
+  // TODO - 4.7
+  // Similar to the one up top, but for likes --
+};
+
+const blogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0,
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0,
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0,
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0,
+  },
+];
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+};
