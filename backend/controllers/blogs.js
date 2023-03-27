a7bef5ebbc0a7725f96886b5549c25546762633c
Junior Love <K4ngar0o@proton.me>
2023-03-27 12:29:00 -0500

    Completed Exercises 4.13-4.14

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,18 +1,45 @@
 const blogRouter = require("express").Router();
+const { update } = require("lodash");
 const Blog = require("../models/blog");
 
-blogRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogRouter.post("/", (request, response) => {
+blogRouter.get("/:id", async (request, response) => {
+  const post = await Blog.findById(request.params.id);
+
+  response.json(post);
+});
+
+blogRouter.post("/", async (request, response) => {
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  const addedBlog = await blog.save();
+
+  response.status(201).json(addedBlog);
+});
+
+blogRouter.delete("/:id", async (request, response) => {
+  const id = request.params.id;
+
+  await Blog.findByIdAndDelete(id);
+  response.status(204).end();
+});
+
+blogRouter.put("/:id", async (request, response) => {
+  const { likes, author, title, url } = request.body;
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    { likes, author, url, title },
+    {
+      new: true,
+    }
+  );
+
+  response.json(updatedBlog);
 });
 
 module.exports = blogRouter;
--- backend/tests/blog-api.test.js
+++ backend/tests/blog-api.test.js
@@ -48,6 +48,34 @@ test("3 - Successful Post", async () => {
 // TODO 4.12
 // Write tests related to creating new blogs via the /api/blogs endpoint, that verify that if the title or url properties are missing from the request data, the backend responds to the request with the status code 400 Bad Request. Make the required changes to the code so that it passes the test.
 
+test("6 - Delete Post", async () => {
+  const id = `6421cba3ec83c9065f34c956`;
+
+  const initialGet = await api.get(`/api/blogs/${id}`).expect(200);
+
+  await api.delete(`/api/blogs/${id}`).expect(204);
+
+  const finalGet = await api.get(`/api/blogs/${id}`);
+  expect(finalGet.body).toBeNull();
+});
+
+test("7 - Update Post", async () => {
+  const random = Math.floor(Math.random() * 10000);
+  const id = `64133d11b70b21c10dac3fd8`;
+  const previousPost = await api.get(`/api/blogs/${id}`).expect(200);
+
+  const updatedPost = {
+    ...previousPost.body,
+    likes: random,
+  };
+
+  const response = await api
+    .put(`/api/blogs/${id}`)
+    .send(updatedPost)
+    .set("Content-Type", "application/json")
+    .expect(200);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
