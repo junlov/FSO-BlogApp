5d426a446040426d33abdc4183f16f01f38645a1
Junior Love <K4ngar0o@proton.me>
2023-03-27 11:03:53 -0500

    Completed Exercises 4.8-4.10

--- backend/tests/blog-api.test.js
+++ backend/tests/blog-api.test.js
@@ -3,16 +3,50 @@ const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
 const Blog = require("../models/blog");
+const logger = require("../utils/logger");
 
-test("Get all blogs from database", () => {
+test("1 - Find Exact Blog", async () => {
   const myBlog = {
-    title: "Gangstas R 4 Eva",
     author: "Tupac Nokur",
+    title: "Gangstas R 4 Eva",
+    id: "6421b3190068e96adcf3475e",
     url: "https://www.tupacsthots.com",
     likes: 1170,
   };
-  api.get("/api/blogs").expect(response.body).toContain(myBlog);
+
+  const res = await api.get("/api/blogs");
+  expect(res.body).toContainEqual(myBlog);
 });
+
+test("2 - Verify UUID is id", async () => {
+  const res = await api.get("/api/blogs");
+  expect(res.body[0].id).toBeDefined();
+});
+
+test("3 - Successful Post", async () => {
+  const initialGet = await api.get("/api/blogs");
+
+  const random = Math.floor(Math.random() * 100);
+
+  const newPost = {
+    author: "Sample Author",
+    title: `Sample Title #${random}`,
+    url: "Sample Url",
+    likes: 1111,
+  };
+
+  await api.post("/api/blogs").send(newPost).expect(201);
+
+  const finalGet = await api.get("/api/blogs");
+
+  expect(finalGet.body).toHaveLength(initialGet.body.length + 1);
+});
+
+// TODO 4.11
+//Write a test that verifies that if the likes property is missing from the request, it will default to the value 0. Do not test the other properties of the created blogs yet. Make the required changes to the code so that it passes the test.
+
+// TODO 4.12
+// Write tests related to creating new blogs via the /api/blogs endpoint, that verify that if the title or url properties are missing from the request data, the backend responds to the request with the status code 400 Bad Request. Make the required changes to the code so that it passes the test.
 
 afterAll(async () => {
   await mongoose.connection.close();
